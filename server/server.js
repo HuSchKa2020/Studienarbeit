f1386dfff68e1938e2ce2779335d424a7dd450ea
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-01-26 16:29:41 +0100

    Initial Commit: server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//const pool = require("./db");
+
+port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`server has started on port ${port}`);
+});
