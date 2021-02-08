5dceee7f0c6a433252931b153e7175612a75dd09
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-02-08 12:03:39 +0100

    delete Fehler
    wenn der Fehler der gelöscht werden sollte nicht in der Datenbank existiert, wird das über die anzahlt der zeilen geprüft und auch zurückgegeben

--- server/routes/fehler.js
+++ server/routes/fehler.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
-//const pool = require("./db");
+const pool = require("../db");
 
 router.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteFehler = await pool.query(
+      "DELETE FROM fehler WHERE fehlerid = $1;",
+      [id]
+    );
+    res.status(200);
+
+    if (deleteFehler.rowCount > 0) {
+      res.json({ error: false, message: `Fehler: ${id} gelöscht!` });
+    } else {
+      res.json({ error: true, message: `Fehler: ${id} existiert nicht!` });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 module.exports = router;
