649e8fc6927c90c0c3785f8c8c2a52457d75651f
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-02-15 16:31:30 +0100

    User Login serverseitig

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-//const pool = require("./db");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const pool = require("./db");
 
 port = 5000;
 
@@ -9,9 +12,38 @@ port = 5000;
 app.use(cors());
 app.use(express.json());
 
+/*
+ToDo: genauer ueber express-session und cookie parser informieren
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUnitialized: true,
+  })
+);
+app.use(cookieParser("secretcode"));
+*/
+
+app.use(passport.initialize());
+app.use(passport.session());
+require("./passportConfig")(passport);
+
 // Import routes
 app.use("/fehler", require("./routes/fehler"));
 app.use("/user", require("./routes/user"));
+
+// Login Route
+app.post("/user/login", (req, res, next) => {
+  console.log("hier");
+  passport.authenticate("local", (err, user, info) => {
+    console.log("hier");
+    if (err) throw err;
+    if (!user) res.send("Email oder Passwort sind falsch");
+    else {
+      res.send("Erfolgreich angemeldet!");
+    }
+  })(req, res, next);
+});
 
 app.listen(port, () => {
   console.log(`server has started on port ${port}`);
