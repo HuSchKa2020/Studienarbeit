2856a487615772eb989029d528edffacab428d17
Jan1208 <70637384+Jan1208@users.noreply.github.com>
2021-02-23 13:12:12 +0100

    Logout funktion in der Toolbar

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,3 +3,4 @@ export const ROOT_URL = "http://localhost:5000";
 // Login und Registrierung
 export const URL_POST_LOGIN = ROOT_URL + "/user/login";
 export const URL_GET_USER = ROOT_URL + "/user/";
+export const URL_GET_LOGOUT = ROOT_URL + "/user/logout";
