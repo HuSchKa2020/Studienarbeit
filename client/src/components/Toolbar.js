f8f532278d31a05c992365f2596244ba809bedcc
derSchumann <der.schumann194@gmx.de>
2021-04-13 15:09:51 +0200

    Menü in der Toolbar
    ein und ausklappbar, aber statisch erstmal eingeklappt

--- client/src/components/Toolbar.js
+++ client/src/components/Toolbar.js
@@ -6,7 +6,18 @@ import { URL_GET_LOGOUT } from "../constants";
 import Axios from "axios";
 import { useHistory } from "react-router-dom";
 
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { MenuData } from "./MenuData";
+import { IconContext } from "react-icons";
+import "./Menu.css";
+
 const Toolbar = () => {
+  const [sidebar, SetSidebar] = useState(false);
+
+  const showSidebar = () => SetSidebar(!sidebar);
+
   const history = useHistory();
 
   const logout = () => {
@@ -26,6 +37,35 @@ const Toolbar = () => {
 
   return (
     <div className="toolbar">
+      <div className="toolbar-menu">
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <div className="Menu">
+            <Link to="#" className="Menu-Leiste">
+              <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+          </div>
+          <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              <li className="navbar-toggle">
+                <Link to="#" className="menu-bars">
+                  <AiIcons.AiOutlineClose />
+                </Link>
+              </li>
+              {MenuData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.titel}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </IconContext.Provider>
+      </div>
+
       <div className="Navigation">
         <FontAwesomeIcon
           icon={faSearch}
@@ -40,7 +80,6 @@ const Toolbar = () => {
           onClick={() => goTo("/fehler/erstellen")}
         />
       </div>
-
       <FontAwesomeIcon
         icon={faUser}
         size="2x"
